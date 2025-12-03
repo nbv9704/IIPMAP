fbcfd58d1732c52021f77c50b362f715de30ce6b
vng004 <giaothot2004@gmail.com>
2025-12-03 18:16:09 +0700

    fix: sửa import store

--- src/components/providers/ClientProviders.tsx
+++ src/components/providers/ClientProviders.tsx
@@ -5,8 +5,8 @@
 'use client'
 
 import { Provider } from "react-redux";
-import store from "@/redux/store";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { store } from "@/redux/store";
 
 interface ClientProvidersProps {
   children: React.ReactNode;
